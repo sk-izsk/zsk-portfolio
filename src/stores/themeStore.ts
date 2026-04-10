3fb23b6a58f89ea68d620f88f5832f1f55c53d03
Zeeshan <sk.zeeshan1992@gmail.comn>
2026-04-10 16:20:24 -0400

    fix default theme from white

--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -3,7 +3,7 @@ import { create } from 'zustand'
 export type ThemeColor = 'color-1' | 'color-2' | 'color-3' | 'color-5' | 'color-7' | 'color-9'
 
 const DEFAULT_THEME_COLOR: ThemeColor = 'color-1'
-const DEFAULT_DARK_MODE = true
+const DEFAULT_DARK_MODE = false
 
 const isThemeColor = (value: string): value is ThemeColor => {
   return (
